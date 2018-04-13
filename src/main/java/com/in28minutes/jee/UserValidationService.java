f552cf2998e7555a4030a97634be71fac2341648
utsav3 <utsav.dhungel@gmail.com>
2018-04-13 12:11:11 -0500

    Changes in User Validation

--- src/main/java/com/in28minutes/jee/UserValidationService.java
+++ src/main/java/com/in28minutes/jee/UserValidationService.java
@@ -3,9 +3,7 @@ package com.in28minutes.jee;
 public class UserValidationService {
 	
 	public boolean isUserValid(String user, String password){
-		if(user.equals("Utsav")&& password.equals("password"))
-			return true;
-		return false;
+		return user.equals("Utsav") && password.equals("password");
 	}
 
 }
